e44718c4d5fa9cea7846c13ec225e659a80714b9
YalukR <yalukramos@gmail.com>
2026-07-19 01:32:16 -0600

    setup reescrito

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,19 +3,29 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./pages/setup/setup.page').then(m => m.SetupPage)
+    loadComponent: () => import('./pages/setup/setup.component').then(m => m.SetupComponent),
+    data: { icon: 'pi-home', label: 'Inicio' },
   },
   {
     path: 'clean',
-    loadComponent: () => import('./pages/clean/clean.page').then(m => m.CleanPage)
+    loadComponent: () => import('./pages/clean/clean.page').then(m => m.CleanPage),
+    data: { icon: 'pi-plus-circle', label: 'Registrar' },
   },
   {
     path: 'history',
-    loadComponent: () => import('./pages/history/history.page').then(m => m.HistoryPage)
+    loadComponent: () => import('./pages/history/history.page').then(m => m.HistoryPage),
+    data: { icon: 'pi-list', label: 'Historial' },
   },
   {
     path: 'stats',
-    loadComponent: () => import('./pages/stats/stats.page').then(m => m.StatsPage)
+    loadComponent: () => import('./pages/stats/stats.page').then(m => m.StatsPage),
+    data: { icon: 'pi-chart-bar', label: 'Estadísticas' },
   },
-  { path: '**', redirectTo: '' }
+  {
+    path: 'settings',
+    loadComponent: () => import('./pages/settings/settings.component').then(m => m.SettingsComponent),
+    data: { icon: 'pi-cog', label: 'Configuraciones'}
+    // sin `data.icon` -> no aparece en el footer
+  },
+  { path: '**', redirectTo: '' },
 ];
