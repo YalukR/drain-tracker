93ac90200c9e69851d3e6df85a5bffe626568bcd
YalukR <yalukramos@gmail.com>
2026-07-19 01:33:51 -0600

    componente de configuraciones de la app

--- src/app/pages/settings/settings.component.ts
+++ src/app/pages/settings/settings.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, signal, computed, inject } from '@angular/core';
+import { ThemeComponent } from './theme/theme.component';
+import { StorageService } from '../../core/services/storage.service';
+import { SetupComponent } from '../setup/setup.component';
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [],
+  imports: [ThemeComponent, SetupComponent],
   templateUrl: './settings.component.html',
-  styleUrl: './settings.component.scss'
 })
 export class SettingsComponent {
 
