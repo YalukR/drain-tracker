415ea125b4db7a2674c81a27d971ede19a666636
YalukR <yalukramos@gmail.com>
2026-07-19 01:32:48 -0600

    footer de la app

--- src/app/layout/page-footer/page-footer.component.ts
+++ src/app/layout/page-footer/page-footer.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject, ChangeDetectionStrategy } from '@angular/core';
+import { RouterModule, NavigationEnd, Router } from '@angular/router';
+import { filter, map, startWith } from 'rxjs/operators';
+import { toSignal } from '@angular/core/rxjs-interop';
+
+interface NavItem {
+  path: string;
+  icon: string;
+  label: string;
+}
 
 @Component({
   selector: 'app-page-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './page-footer.component.html',
-  styleUrl: './page-footer.component.scss'
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PageFooterComponent {
+  private router = inject(Router);
 
+  items: NavItem[] = this.router.config
+    .filter(route => route.data?.['icon'] && route.data?.['label'])
+    .map(route => ({
+      path: `/${route.path}`,
+      icon: route.data!['icon'],
+      label: route.data!['label'],
+    }));
+
+  active = toSignal(
+    this.router.events.pipe(
+      filter(e => e instanceof NavigationEnd),
+      map(e => (e as NavigationEnd).urlAfterRedirects),
+      startWith(this.router.url)
+    ),
+    { initialValue: this.router.url }
+  );
 }
